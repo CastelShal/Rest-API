de31041d3b85f0e0a9b3426ca5fd5b00af6beedc
NikthePhoenix <nikgov23@gmail.com>
2024-02-13 15:39:04 +0530

    partial mq producer integration

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 import express from "express";
-
+import bodyParser from "body-parser";
 import { sequelize } from "./connect.js";
 import Tag from "./models/tags.js";
 import User from "./models/user.js";
 import BookedEvents from "./models/bookedEvents.js";
-
 import userRouter from "./routes/users.js";
 import bookEventsRouter from "./routes/bookedEvents.js";
 import organizerRouter from "./routes/organizer.js";
 import eventsRouter from "./routes/events.js";
 import Organizer from "./models/organiser.js";
 import Event from "./models/event.js";
+import Producer from "./producer.js";
 
 const app = express();
 const PORT = 3000;
+const producer = new Producer();
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(bodyParser.json("application/json"));
+
+app.post("/sendMail", async (req, res, next) => {
+  await producer.publishMessage(req.body.mailType, req.body.to, req.body.subject, req.body.body);
+  res.send();
+});
+
 
 //router mounts
 app.use("/user", userRouter);
--- connect.js
+++ connect.js
@@ -1,20 +1,20 @@
 import config from "./config.js";
 import { Sequelize, Op } from "sequelize";
 
-export const sequelize = new Sequelize(
-  config.db,
-  config.user,
-  config.password,
-  {
-    host: config.host,
-    dialect: config.dialect,
-    // operatorsAliases: false,
-    pool: config.pool,
-    logging: false,
-  }
-);
+// export const sequelize = new Sequelize(
+//   config.db,
+//   config.user,
+//   config.password,
+//   {
+//     host: config.host,
+//     dialect: config.dialect,
+//     // operatorsAliases: false,
+//     pool: config.pool,
+//     logging: false,
+//   }
+// );
 
-/*export const sequelize = new Sequelize({
+export const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "database.sqlite",
-});*/
+});
