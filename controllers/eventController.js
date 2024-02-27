2b3bd581a8f84a0c3f97b7d93d41202aaecb283b
Castel <castelshal@gmail.com>
2024-02-27 14:39:21 +0530

    Hotfix

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -2,46 +2,66 @@ import Organizer from "../models/organizer.js";
 import Event from "../models/event.js";
 import { Op } from "sequelize";
 import { sequelize } from "../connect.js";
+import BookedEvents from "../models/bookedEvent.js"
 
 export async function getEventByDepartment(req, res) {
-    try {
-      const event = await Event.findAll({
-        include: [
-          { model: Organizer, as: "organizer" },
-          { model: Organizer, as: "collaborator" }
-        ],
-        where: {
-          orgId: await getOrgId(req.params.department),
-        },
-      });
-      res.status(200).json(event);
-    } catch (e) {
-      console.error(e);
-      res.sendStatus(500);
-    }
+  try {
+    const event = await Event.findAll({
+      include: [
+        { model: Organizer, as: "organizer" },
+        { model: Organizer, as: "collaborator" }
+      ],
+      where: {
+        orgId: await getOrgId(req.params.department),
+      },
+    });
+    res.status(200).json(event);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
   }
+}
 
-  async function getOrgId(department) {
-    try {
-      const dept = await Organizer.findOne({
-        attributes: ["orgId"],
-        where: {
-          orgDept: department,
-        },
-      });
-      return dept.orgId;
-    } catch (e) {
-      console.error(e);
-    }
+async function getOrgId(department) {
+  try {
+    const dept = await Organizer.findOne({
+      attributes: ["orgId"],
+      where: {
+        orgDept: department,
+      },
+    });
+    return dept.orgId;
+  } catch (e) {
+    console.error(e);
   }
+}
 
-  export async function getEventByName(req, res) {
-    try {
-      const event = await Event.findAll({
-        include: [
-          { model: Organizer, as: "organizer"},
-          {model: Organizer, as: "collaborator"}
+export async function getEventByName(req, res) {
+  try {
+    const event = await Event.findAll({
+      include: [
+        { model: Organizer, as: "organizer" },
+        { model: Organizer, as: "collaborator" }
+      ],
+      where: {
+        [Op.and]: [
+          sequelize.where(sequelize.fn("lower", sequelize.col("eventName")), {
+            [Op.like]: `%${req.params.eventName.toLowerCase()}%`,
+          }),
         ],
+      },
+    });
+    res.status(200).json(event);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+}
+
+export async function deleteEvent(req, res) {
+  try {
+    const event = await Event.findOne(
+      {
         where: {
           [Op.and]: [
             sequelize.where(sequelize.fn("lower", sequelize.col("eventName")), {
@@ -49,45 +69,54 @@ export async function getEventByDepartment(req, res) {
             }),
           ],
         },
-      });
-      res.status(200).json(event);
-    } catch (e) {
-      console.error(e);
-      res.sendStatus(500);
-    }
+      }
+    );
+    await BookedEvents.destroy({
+      where: {
+        eventId: event.eventId,
+      }
+    })
+    event.destroy();
+    res.sendStatus(200);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
   }
+}
 
-  export async function deleteEvent(req, res) {
-    try {
-      await Event.destroy({
-        where: {
-          eventName: req.params.eventName,
-        },
-      });
-      res.sendStatus(200);
-    } catch (e) {
-      console.error(e);
-      res.sendStatus(500);
-    }
+export async function getAllEvents(req, res) {
+  try {
+    const event = await Event.findAll({
+      include: [
+        { model: Organizer, as: "organizer" },
+        { model: Organizer, as: "collaborator" }
+      ]
+    });
+    res.status(200).json(event);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
   }
+}
 
-  export async function getAllEvents(req, res) {
-    try {
-      const event = await Event.findAll({
-        include: [
-          { model: Organizer, as: "organizer"},
-          {model: Organizer, as: "collaborator"}
-        ]
-      });
-      res.status(200).json(event);
-    } catch (e) {
-      console.error(e);
-      res.sendStatus(500);
-    }
-  }
+export async function setEvent(req, res) {
+  const {
+    eventId,
+    orgId,
+    tagId,
+    eventName,
+    eventDateTime,
+    eventVenue,
+    maxCapacity,
+    eccPoints,
+    description,
+    collaborator1,
+    collaborator2,
+    url,
+  } = req.body;
 
-  export async function setEvent(req, res) {
-    const {
+  try {
+    await Event.create({
       eventId,
       orgId,
       tagId,
@@ -100,48 +129,32 @@ export async function getEventByDepartment(req, res) {
       collaborator1,
       collaborator2,
       url,
+    });
+    res.sendStatus(201);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(422);
+  }
+}
+
+export async function updateEvent(req, res) {
+  const id = req.body.eventId;
+  try {
+    const event = await Event.findByPk(id);
+    const { eventName, eventDateTime,
+      eventVenue, maxCapacity, eccPoints,
+      description, colaborator1, url
     } = req.body;
 
-    try {
-      await Event.create({
-        eventId,
-        orgId,
-        tagId,
-        eventName,
-        eventDateTime,
-        eventVenue,
-        maxCapacity,
-        eccPoints,
-        description,
-        collaborator1,
-        collaborator2,
-        url,
-      });
-      res.sendStatus(201);
-    } catch (e) {
-      console.error(e);
-      res.sendStatus(422);
-    }
+    event.update({
+      eventName, eventDateTime,
+      eventVenue, maxCapacity, eccPoints,
+      description, colaborator1, url
+    });
+    res.sendStatus(200);
   }
-
-  export async function updateEvent(req, res) {
-    const id = req.body.eventId;
-    try {
-      const event = await Event.findByPk(id);
-      const { eventName, eventDateTime,
-        eventVenue, maxCapacity, eccPoints,
-        description, colaborator1, url
-      } = req.body;
-  
-      event.update({
-        eventName, eventDateTime,
-        eventVenue, maxCapacity, eccPoints,
-        description, colaborator1, url
-      });
-      res.sendStatus(200);
-    }
-    catch (e) {
-      console.error(e);
-      res.sendStatus(500);
-    }
+  catch (e) {
+    console.error(e);
+    res.sendStatus(500);
   }
+}
